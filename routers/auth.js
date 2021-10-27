c2007a46cd7380fbb4e7fb37ea7c44cbaaa6b0c2
fernandofernandessimao <fernandofernandessimao@gmail.com>
2021-10-27 09:02:15 +0200

    Reset database

--- routers/auth.js
+++ routers/auth.js
@@ -66,37 +66,6 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-// router.post("/auction", async (req, res) => {
-//   const { title, imageUrl, minimumBid } = req.body;
-
-//   if (!title || !imageUrl || !minimumBid) {
-//     return res
-//       .status(400)
-//       .send("Please provide an title, imageUrl and a minimum bid");
-//   }
-//   try {
-//     const newArtwork = await Artwork.create({
-//       title,
-//       userId: 1,
-//       imageUrl,
-//       minimumBid,
-//     });
-
-//     res.status(201).send(...newArtwork);
-//   } catch (error) {
-//     if (error.name === "SequelizeUniqueConstraintError") {
-//       return res
-//         .status(400)
-//         .send({ message: "There is an existing account with this email" });
-//     }
-
-//     return res.status(400).send({ message: "Something went wrong, sorry" });
-//   }
-// });
-
-// The /me endpoint can be used to:
-// - get the users email & name using only their token
-// - checking if a token is (still) valid
 router.get("/me", authMiddleware, async (req, res) => {
   // don't send back the password hash
   delete req.user.dataValues["password"];
